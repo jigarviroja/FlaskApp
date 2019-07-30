ae307360b0251afa907b3a53b95e943164495000
jigarviroja <jigarviroja@gmail.com>
2019-07-30 13:39:07 +0530

    Database Connected in main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    """This function retrive srno,name,email,phone_num,msg and date from the databe"""
+    srno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(20),  nullable=False)
+    phone_num = db.Column(db.String(12),  nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
 
 
 @app.route("/")
@@ -12,8 +27,20 @@ def about():
     return render_template('about.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if(request.method == 'POST'):
+        '''Add entry to database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contacts(name=name, phone_num=phone,
+                         date=datetime.now(), msg=message, email=email)
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template('contact.html')
 
 
